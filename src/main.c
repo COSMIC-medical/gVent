2a8fab4fc4b2c355c2c453aa383b28c26ab4967b
Simon Diemert <simon.diemert@gmail.com>
2020-10-28 21:24:16 +0000

    Initialize GPIO for valves

--- src/main.c
+++ src/main.c
@@ -26,6 +26,7 @@
 #include "application/dss.h"
 #include "platform/scheduler.h"
 #include "platform/scheduler_private.h"
+#include "platform/configuration_private.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -275,7 +276,37 @@ static void MX_TIM3_Init(void)
   */
 static void MX_GPIO_Init(void)
 {
-  // TODO - implement GPIO initializations here...
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /* Set default GPIO pin Output Level */
+  HAL_GPIO_WritePin(INSP_VALVE_PORT, INSP_VALVE_PIN, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(EXP_VALVE_PORT, EXP_VALVE_PIN, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(TANK_VALVE_PORT, TANK_VALVE_PIN, GPIO_PIN_RESET);
+
+  /* Configure each GPIO pin */
+  GPIO_InitStruct.Pin = INSP_VALVE_PIN;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
+  HAL_GPIO_Init(INSP_VALVE_PORT, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin = EXP_VALVE_PIN;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
+  HAL_GPIO_Init(EXP_VALVE_PORT, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin = TANK_VALVE_PIN;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
+  HAL_GPIO_Init(TANK_VALVE_PORT, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 4 */
