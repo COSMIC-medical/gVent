66eb5422eec8d4d81df3ba607a739cb62577f7f5
Patrick Wilkie <patrickwilkie@Patricks-MacBook-Pro.local>
2020-10-29 16:55:47 -0700

    Added Byte Commands for fs6122 to _private.h

--- include/platform/pressureFlowSensor_private.h
+++ include/platform/pressureFlowSensor_private.h
@@ -78,6 +78,31 @@ typedef struct MeasurementsStatus {
 	int last_expiratory_flow_read_time;
 } measurements_status_t;
 
+
+/*=========================================================================
+    I2C ADDRESS/BITS
+    -----------------------------------------------------------------------*/
+#define FS6122_DEFAULT_ADDRESS                 (0x1D)
+/*=========================================================================*/
+
+/////////////////////////////////////
+// Hex Byte Commands for fs6122
+// WRITE COMMANDS
+#define FS6122_WRITE_ADDRESS            0x05 // write the i2c address, Bit7 ~ Bit1 can be set in hex (0x08 means address of 4)
+#define FS6122_WRITE_FILTERDEPTH        0x0B // write the filter depth Int8, 0-254
+#define FS6122_CAL_FLOWRATE_OFFSET      0x1C // calibration the offset of the flow rate, 1 byte, any value, ensure NO flow in the pipe
+#define FS6122_CAL_PRESSURE_OFFSET      0x24 // calibration of offset of pressure, 1 byte, any value, ensure NO flow in the pipe
+
+// READ COMMANDS
+#define FS6122_READ_SENSOR_SN           0x82 // ASCII
+#define FS6122_READ_FLOWRATE            0x83 // Int32/1000 SLPM
+#define FS6122_READ_FLOWRATE_PRESSURE   0x84 // Int32/1000 SLPM, int32/1000 cmH20
+
+#define FS6122_READ_ADDRESS             0x85 // Bit7 ~ Bit1
+#define FS6122_READ_FILTERDEPTH         0x8B // Int8, 0~254
+#define FS6122_READ_PRESSURE            0xA3 // Int32/1000 cmH20
+#define FS6122_READ_TEMPERATURE         0x82  // Int16/100 deg celcius
+#define FS6122_READ_HUMIDITY            0x83  // Int16/100 %RH
+
+
 #endif /* INC_PLATFORM_MEASURED_INPUTS_H_ */
-
-
