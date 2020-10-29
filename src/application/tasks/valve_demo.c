0f45c2d18979041b0d1dba635fef873cededf51e
Simon Diemert <simon.diemert@gmail.com>
2020-10-29 02:17:41 +0000

    Implement valve control demo task

--- src/application/tasks/valve_demo.c
+++ src/application/tasks/valve_demo.c
@@ -1,5 +1,105 @@
+#include <application/dss.h>
 #include <application/tasks/valve_demo.h>
+#include <platform/common.h>
+#include <platform/valve.h>
+#include <platform/systemInfo.h>
 
+// number of milliseconds to wait between valve actuations
+uint32_t wait_time_ms = 5000;
+
+// current state of our internal task state machine
+uint32_t state = 0;
+
+// last time a valve state was changed
+uint32_t last = 0;
+
+/**
+ * This task runs every 50 ms. However, the requirements
+ * for this task are to control the valves on the order
+ * of seconds. So we must use a state machine that is updated
+ * between invocations of this task. 
+ * 
+ * Note that this is a demo implementation only. Only to be
+ * used for testing valve actuation/hookup.
+ */
 void task_valve_demo() {
-    // TODO - implement me
+
+    /* fetch the current time in milliseconds
+     *
+     * in this demo implementation, we do not account for timer overflow
+     * which would occur in a long running application after approximately 
+     * (2^32)/(1000*60*60*24) = ~49 days of continous operation without
+     * a syste reset.
+     */ 
+    uint32_t now = get_current_time();
+
+    switch(state) {
+
+        case 0:
+            // initial state, all valves closed
+            close_inspiratory_valve();
+            close_expiratory_valve();
+            close_tank_valve();
+            state = 1;
+            break;
+
+        case 1:
+            // open the inspiratory valve and set
+            // a timer variable
+            open_inspiratory_valve();
+            last = get_current_time();
+            state = 2;
+            break;
+
+        case 2:
+            // wait for 5 seconds while inspiratory valve is open
+            // once elapsed, close inspiratory valve and advance
+            if (now - last > wait_time_ms) {
+                close_inspiratory_valve();
+                state = 3;
+            }
+            break;
+
+        case 3:
+            // close inspiratory valve and open expiratory valve
+            // also reset timer variable
+            open_expiratory_valve();
+            last = get_current_time();
+            state = 4;
+            break;
+
+        case 4:
+            // wait for 5 seconds while the expiratory valve is open
+            // once elapsed, close expiratory valve and advance
+            if (now - last > wait_time_ms) {
+                close_expiratory_valve();
+                state = 5;
+            }
+            break;
+
+        case 5:
+            // open tank valve and set a timer variable
+            open_tank_valve();
+            last = get_current_time();
+            state = 6;
+            break;
+
+        case 6:
+            // wait 5 seconds while tank valve is open
+            // once elapsed, then close tank valve and
+            // loop back to initial state.
+            if (now - last > wait_time_ms) {
+                close_tank_valve();
+                state = 0;
+            }
+            break;
+
+        default:
+            // should never reach this state, if we do something
+            // bad has happened; transition to DSS.
+            dss();
+            break;
+
+    }
+
 }
