56413dd50a1966f38dec0f94028d357dec03abf6
sdiemert <simon.diemert@gmail.com>
2020-11-03 13:47:41 -0800

    Update alarm definitions

--- include/platform/alarm.h
+++ include/platform/alarm.h
@@ -1,37 +1,53 @@
 /*
- * pin.h
+ * alarm.h
  *
- * Functions an application developer will used to interact with the alarm.
- *
- * To be re-implemented on COSMIC hooks up devices.
+ * Functions to interact with the alarm.
  */
 
 #ifndef SRC_PLATFORM_ALARM_H_
 #define SRC_PLATFORM_ALARM_H_
 
+/**
+ * Status of the alarm. 
+ */
+typedef enum {
+    OFF, // buzzer and alarm LED off. 
+    ON, // buzzer and alarm LED both on.
+    MUTED // buzzer off and alarm LED on.
+} alarm_status_t;
+
+/**
+ * @return the current alarm status.
+ */
+alarm_status_t get_alarm_status();
+
 /*
- * Update the alarm status to on
- * Powers on the alarm's led
- * If the alarm is not muted powers on the alarm buzzer
+ * Turn on the alarm. 
+ * 
+ * Powers on the alarm's LED, and if the alarm is not 
+ * muted, powers on the alarm buzzer
  */
 void set_alarm_on();
 
 /*
- * Update the alarm status to on
- * Powers off the alarm's led
- * Powers off the alarm's buzzer
+ * Turn off the alarm. 
+ * 
+ * Powers off the alarm's LED and turns off the alarm's
+ * buzzer.
  */
 void set_alarm_off();
 
 /*
  * Updates the alarm status to muted
+ * 
  * If the alarm is on powers off the alarm's buzzer
  */
 void mute_alarm();
 
 /*
  * Updates the alarm status to not muted
- * If the alarm is on powers on the alarm's buzzer
+ * 
+ * If the alarm is on powers on the alarm's buzzer.
  */
 void unmute_alarm();
 
