d98f63dde28fb0401901d13d0bf53d0c6df248d4
sdiemert <simon.diemert@gmail.com>
2020-11-04 15:10:48 -0800

    Update hardware initialization for buzzer

--- include/platform/init.h
+++ include/platform/init.h
@@ -8,12 +8,29 @@
 #ifndef INC_PLATFORM_INIT_H_
 #define INC_PLATFORM_INIT_H_
 
+void init_status_gpio(void);
+
 /**
  * Initialize the general purpose input/output ports
- * of the microprocessor. When this function completes
- * all of the required GPIO for the platform has been
- * initialized.
+ * of the microprocessor for the purpose of actuating
+ * valves. 
+ * 
+ * When this function completes all of the hardware
+ * peripherals for the valve control has initialized.
  */
-void  init_gpio(void);
+void init_valve_gpio(void);
+
+/**
+ * Initialize the hardware peripherals used to actuate
+ * the alarms. Includes setting up the alarm LED and
+ * also setting up the buzzer output in PWM mode.
+ * 
+ * When this function completes the GPIO for the LED
+ * is setup.
+ * 
+ * When the this function completes a timer and GPIO 
+ * is setup for the alarm buzzer.
+ */
+void init_alarm(void);
 
 #endif /* INC_PLATFORM_INIT_H_ */
