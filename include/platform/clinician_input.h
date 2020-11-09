d5aa68050c441ae9f1ec143a01ee807abed337ed
Victor Chiew <victor.yhchiew@gmail.com>
2020-11-08 16:37:04 -0800

    finished draft breath cycle calc test

--- include/platform/clinician_input.h
+++ include/platform/clinician_input.h
@@ -8,8 +8,7 @@
  * todo add a comment here
  *
  */
-Validity_Status get_respiratory_rate(uint32_t * RR); // Victor: Should this be something like respiratory_rate_validity? Or should the check and the 
-                                                    //          value retrieval be in the same function? 
+enum Validity_Status get_respiratory_rate(uint32_t * RR); // Victor: Should this be something like respiratory_rate_validity? Or should the check and the value retrieval be in the same function? 
 
 int retrieve_RR(){
     return 15;
