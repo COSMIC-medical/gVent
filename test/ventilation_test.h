2304c550fd2dec17e7cb277798a22462fab56bfb
Victor Chiew <victor.yhchiew@gmail.com>
2020-11-08 16:37:04 -0800

    finished draft breath cycle calc test

--- test/ventilation_test.h
+++ test/ventilation_test.h
@@ -6,3 +6,5 @@
 int startVentilation_afterOneInspiration_openInspiratoryValve();
 
 int startVentilation_duringInspiration_doesNotopenInspiratoryValve();
+
+static int calculateBreathCycleDuration_15BPM();
