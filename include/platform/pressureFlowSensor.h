7511a0390a9c3a5429e88be1636f4f2c97a65867
RayhanBosch <rayhan.bosch1996@gmail.com>
2020-11-17 21:09:06 -0800

    Validation for inspiratory/expiratory flows

--- include/platform/pressureFlowSensor.h
+++ include/platform/pressureFlowSensor.h
@@ -10,12 +10,17 @@
 #ifndef INC_PLATFORM_PRESSURE_FLOW_SENSOR_H_
 #define INC_PLATFORM_PRESSURE_FLOW_SENSOR_H_
 
+uint32_t read_FS6122_sensor(uint8_t, uint8_t);
+
 /*
  * Reads and validates the read inspiratory pressure
  * Updates the measurement inputs
  *
- * returns the inspiratory pressure in ??? when it is valid
- * returns ??? when no valid pressure is available
+ *  Validation: 
+ *      -5 ~ +40 [cmH2O]
+ * 
+ * returns the inspiratory pressure in cmH2O when it is valid
+ * returns cmH2O when no valid pressure is available
  *
  */
 int get_inspiratory_pressure();
@@ -23,9 +28,12 @@ int get_inspiratory_pressure();
 /*
  * Reads and validates the read expiratory pressure
  * Updates the measurement inputs
+ * 
+ * Validation: 
+ *      -5 ~ +40 [cmH2O]
  *
- * returns the expiratory pressure in ??? when it is valid
- * returns ??? when no valid pressure is available
+ * returns the expiratory pressure in cmH2O when it is valid
+ * returns cmH2O when no valid pressure is available
  */
 int get_expiratory_pressure();
 
@@ -33,17 +41,23 @@ int get_expiratory_pressure();
  * Reads and validates the read inspiratory flow
  * Updates the measurement inputs
  *
- * returns the inspiratory flow in ??? when it is valid
- * returns ??? when no valid flow is available
+ * Validation: 
+ *      flow between -250 ~ +250 SLPM
+ * 
+ * returns the inspiratory flow in SLPM when it is valid
+ * returns SLPM when no valid flow is available
  */
 int get_inspiratory_flow();
 
 /*
  * Reads and validates the read expiratory flow
  * Updates the measurement inputs
+ * 
+ * Validation: 
+ *      flow between -250 ~ +250 SLPM
  *
- * returns the expiratory flow in ??? when it is valid
- * returns ???  when no valid flow is available
+ * returns the expiratory flow in SLPM when it is valid
+ * returns SLPM when no valid flow is available
  */
 int get_expiratory_flow();
 
